1a7ddcfc1dab7bc3a47c484b775be24fd051b1d1
Joel Kwartler <lojoel3+gh@gmail.com>
2021-09-29 13:53:05 -0700

    before a big refactor

--- src/open-in-figma.ts
+++ src/open-in-figma.ts
@@ -1,7 +1,7 @@
 import * as sourcegraph from 'sourcegraph'
-import { from } from 'rxjs'
-import { filter, switchMap } from 'rxjs/operators'
-import * as stringSimilarity from 'string-similarity'
+// import { from } from 'rxjs'
+// import { filter, switchMap } from 'rxjs/operators'
+// import * as stringSimilarity from 'string-similarity'
 
 export function checkIsURL(maybeURL: string): boolean {
     try {
@@ -12,21 +12,11 @@ export function checkIsURL(maybeURL: string): boolean {
     }
 }
 
-/**
- * Strips trailing comma or period from URL
- *
- * @param maybeURL Possible URL string
- */
-export function cleanURL(maybeURL: string): string {
-    return maybeURL.replace(/[,.]$/, '')
-}
-
 const numMatchWords = 6;
 
-// NEXT: refactor loops to use promise.all() because you have to wait for them to execute
-// Alternatively build a storage service
+// function analyzePageforStrings 
 
-function getFigmaUrl(textDocumentUri: URL, editor: sourcegraph.ViewComponent): URL {
+function getFigmaUrl(textDocumentUri: URL, editor: sourcegraph.CodeEditor): URL {
   console.log("textDocumentUri");
   console.log(textDocumentUri);
   console.log("editor!");
@@ -41,11 +31,12 @@ function getFigmaUrl(textDocumentUri: URL, editor: sourcegraph.ViewComponent): U
   // const rawStringsPattern = />([\w+\s])*</g
   // let's try this one - it'll get words on tnheir own lines
   // const rawStringsPattern = />\n[-\w+\s,.]*\n\s*</g
-  const rawStringsPattern = /=?>\s*\w[-\w+\s,.]*\s*<((\w>)|(\w,))?/g
+  const rawStringsPattern = /(=|Field)?>\s*\w[-\w+\s,.]*\s*<((\w>)|(\w,))?/g
   const encodedStringsPattern = /[\s=]['"]\w+\s[A-Za-z]+\s\w+\s\w+[.,\s\w']*['"]/g
   var searchString = '';
   var cleanMatches = [];
-  // this includes dots which we now need to strip, maybe? 
+  // this includes dots which we now need to strip, maybe?
+  // TODO check if page is defined  
   console.log("got to 40 at " + Date.now())
   var matches = page.match(rawStringsPattern)
   var backupMatches = page.match(encodedStringsPattern);
@@ -56,6 +47,8 @@ function getFigmaUrl(textDocumentUri: URL, editor: sourcegraph.ViewComponent): U
     for (var i = 0; i < matches.length; i++) {
       // effectively delete these things that were ...=>new Map<type> in the code 
       matches[i].startsWith("=>") ? matches[i] = '' : matches[i];
+      // delete things that were <Field>maven.repos</Field> type also 
+      matches[i].startsWith("Field>") ? matches[i] = '' : matches[i];
       // to catch cases likes <something>new export type<p>
       // console.log('matches?');
       // console.log(/<\w>$/.test(matches[i]));
@@ -120,93 +113,11 @@ function getFigmaUrl(textDocumentUri: URL, editor: sourcegraph.ViewComponent): U
     }
   }
 
-  // make lowercase, make unique, sort 
-
-
-
   console.log(searchString);
 
   searchString = encodeURIComponent(searchString).trim()
   const figmaUrl = "https://www.figma.com/files/search?model_type=files&q=" + searchString
-  // console.log("hi!!\n");
-  // const hardcodedSourcegraphString = "This is a doodley text file"
-  // const PERSONAL_ACCESS_TOKEN = '184998-18bab379-276b-4fbf-836c-f712fe458b31';
-  // var fileStringsDict = {};
-  // // TODO probably don't need both these objects
-  // var fileStrings = []; 
-  // // we can assume the "team" is a configurable field in settings config so we get it
-  // // hardcoded mine here for testing
-  // function tryit(){ return fetch('https://api.figma.com/v1/teams/887741539104471731/projects', {
-  //       method: 'GET',
-  //       headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN }
-  //   }).then(function(response) {
-  //       console.log('\nhi');
-  //       response.json().then(teamProjects => {
-  //         // "iterate" through project files TODO – I have only one file right now  
-  //         console.log(teamProjects.projects[1].id)
-  //         // now fetch that project's files 
-  //         fetch('https://api.figma.com/v1/projects/' +teamProjects.projects[1].id + '/files', {
-  //           method: 'GET',
-  //           headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN }
-  //         }).then(function(response) {
-  //           console.log('in files');
-  //           response.json().then(projectFiles => {
-  //             // 
-  //             // FILE LOOP: this loops within files in a project
-  //             //
-  //             for (let j=0; j<projectFiles.files.length; j++) {
-  //               var fileName = projectFiles.files[j].name;
-  //               // https://api.figma.com/v1/files/:file_key/nodes
-  //               var fileKey = projectFiles.files[j].key;
-  //               console.log("iterating through objects for " + fileName + " " + fileKey);
-  //               fetch('https://api.figma.com/v1/files/'+fileKey, {
-  //                 method: 'GET',
-  //                 headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN }
-  //               }).then(function(response) {
-  //                 response.json().then(fileNodes => {
-  //                   // iterate through file nodes and grab all text nodes: 
-  //                   const doc = fileNodes.document;
-  //                   const canvas = doc.children[0];
-  //                   //
-  //                   // FILE OBJECTS LOOP: for each object in a file 
-  //                   //
-  //                   for (let i=0; i<canvas.children.length; i++) {
-  //                     const child = canvas.children[i]
-  //                     console.log(child);
-  //                     // this is where we can do stuff with the file nodes
-  //                     // here I am logging all the characters to do a match 
-  //                     if (child.type === 'TEXT') {
-  //                       console.log(child.characters); 
-  //                       fileStrings.push(child.characters)
-  //                       fileStringsDict[child.characters] = fileKey; 
-  //                     }
-  //                   } // end loop for canvas nodes
-  //                 });
-  //               });
-  //             } // end loop for files within a project 
-  //           });
-  //       // test for similarity
-  //       // this takes strings, but then we have to map strings back to the file identifier 
-  //       // so we need a dictionary of strings + file identifier 
-  //       console.log("calling a similarity function!");
-  //       const similarity = stringSimilarity.findBestMatch(hardcodedSourcegraphString, fileStrings);
-  //       console.log(similarity); 
-  //       // this takes our best match and gets a file name back 
-  //       console.log(fileStringsDict[similarity.bestMatch.target]);
-  //       // TODO how to handle the case when the best match is too loW?? 
-
-  //         }); // todo add catch here
-
-  //       }); 
-  //       ; 
-  //       return true; // response.json();
-  //   }).catch(function (error) {
-  //       console.log(error); 
-  //       return { err: error };
-  //   });
-  // };
   
-  // tryit();
   console.log("got to 109")
   const openUrl = new URL(figmaUrl)
   console.log(openUrl)
@@ -214,8 +125,20 @@ function getFigmaUrl(textDocumentUri: URL, editor: sourcegraph.ViewComponent): U
 }
 
 export function activate(ctx: sourcegraph.ExtensionContext): void {
+  let anyStrings = false
+  // sourcegraph.internal.updateContext({
+  //         [`openInFigma.anyStrings`]: anyStrings,
+  // });
+  // anyStrings = true;
+  // setTimeout(() => {
+  //   sourcegraph.internal.updateContext({
+  //     [`openInFigma.anyStrings`]: anyStrings,
+  // });
+  // }, 5000)
+  // so we want to run the figma analysis on the page and determine if the button should be active
+    
   console.log("acivated now!!!");
-    if (sourcegraph.app.activeWindowChanges) {
+    // if (sourcegraph.app.activeWindowChanges) {
       // TODO, I ripped this from the DD extension is it important? 
       // const activeEditor = from(sourcegraph.app.activeWindowChanges).pipe(
       //     filter((window): window is sourcegraph.Window => window !== undefined),
@@ -225,7 +148,7 @@ export function activate(ctx: sourcegraph.ExtensionContext): void {
       // // When the active editor changes, publish new decorations.
       // // ctx.subscriptions.add(activeEditor)
       ctx.subscriptions.add(
-        sourcegraph.commands.registerCommand('openInFigma.openFigmaLink', async (uri?: string) => {
+        sourcegraph.commands.registerCommand('openInFigma.openFigmaLink', async () => {
             // if (!uri) {
             //     const viewer = sourcegraph.app.activeWindow?.activeViewComponent
             //     uri = viewerUri(viewer)
@@ -235,152 +158,20 @@ export function activate(ctx: sourcegraph.ExtensionContext): void {
             // }
             const activeEditor = () => sourcegraph.app.activeWindow && sourcegraph.app.activeWindow.activeViewComponent
             const editor = activeEditor()
-            // some kind of check like this?  if (editor && editor.type === 'CodeEditor') { 0 
-            const openUrl = getFigmaUrl(new URL("https://figma.com"), editor)
-            console.log("hello out there!!");
-            await sourcegraph.commands.executeCommand('open', openUrl.href)
+            // some kind of check like this?  
+            console.log("editor:");
+            console.log(editor);
+            if (editor && editor.type === 'CodeEditor') {  
+              const openUrl = getFigmaUrl(new URL("https://figma.com"), editor)
+              console.log("hello out there!!");
+              await sourcegraph.commands.executeCommand('open', openUrl.href)
+            }
+            // return void 0
         })
-    )
-  }
+      )
+        // sourcegraph.internal.updateContext({
+        //   [`openInFigma.file.${textDocument.uri}.strings`]: true,
+        // })
+
+  // }
 }
-
-// TODO –– I don't understand why I need this yet... haha...
-function viewerUri(viewer: sourcegraph.ViewComponent | undefined): string | undefined {
-  switch (viewer?.type) {
-      case 'CodeEditor':
-          return viewer.document.uri
-      case 'DirectoryViewer':
-          return viewer.directory.uri.href
-      default:
-          return undefined
-  }
-}
-
-// export function activate(context: sourcegraph.ExtensionContext): void {
-
-//     context.subscriptions.add(
-//         sourcegraph.languages.registerHoverProvider(['*'], {
-//             provideHover: (doc, pos) => ({
-//                 contents: {
-//                     value: 'Hello world from joel open-in-figma! 🎉🎉🎉',
-//                     kind: sourcegraph.MarkupKind.Markdown
-//                 }
-//             }),
-//         })
-//         //     provideHover(document, position) {
-//         //       const PERSONAL_ACCESS_TOKEN = '184998-18bab379-276b-4fbf-836c-f712fe458b31';
-
-//         //       // function apiRequest() {
-//         //         return fetch('https://api.figma.com/v1/teams/887741539104471731/projects', {
-//         //             method: 'GET',
-//         //             headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN }
-//         //         }).then(function(response) {
-//         //             console.log('hi');
-//         //             response.json().then(teamProjects => {
-//         //               // "iterate" through project files TODO 
-//         //               console.log(teamProjects.projects[1].id)
-//         //               // now fetch that project's files 
-//         //               fetch('https://api.figma.com/v1/projects/' +teamProjects.projects[1].id + '/files', {
-//         //                 method: 'GET',
-//         //                 headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN }
-//         //               }).then(function(response) {
-//         //                 console.log('in files');
-//         //                 response.json().then(projectFiles => {
-//         //                   var fileName = projectFiles.files[0].name;
-//         //                   // https://api.figma.com/v1/files/:file_key/nodes
-//         //                   var fileKey = projectFiles.files[0].key;
-//         //                   fetch('https://api.figma.com/v1/files/'+fileKey, {
-//         //                     method: 'GET',
-//         //                     headers: { "x-figma-token": PERSONAL_ACCESS_TOKEN }
-//         //                   }).then(function(response) {
-//         //                     console.log('got file nodes');
-//         //                     console.log(response.text());
-//         //                   });
-//         //                 });
-//         //               }); // todo add catch here
-
-//         //             }); 
-//         //             console.log('logged project');
-//         //             ; 
-//         //             // console.log(response.json());
-//         //             return true; // response.json();
-//         //         }).catch(function (error) {
-//         //             console.log(error); 
-//         //             return { err: error };
-//         //         });
-
-//         //       // apiRequest();
-//         //       // console.log("jk1");
-//         //       //   const range = document.getWordRangeAtPosition(position)
-//         //       //   const word = document.getText(range)
-//         //       //   if (!word) {
-//         //       //       return
-//         //       //   }
-//         //       //   console.log("jk2");
-
-//         //       //   const maybeURL = cleanURL(word)
-
-//         //       //   if (!checkIsURL(maybeURL)) {
-//         //       //       return
-//         //       //   }
-//         //       //   console.log("jk3");
-//         //       //   console.log(maybeURL);
-//         //       //   console.log(range);
-//         //         /**
-//         //          * Creates the markdown string to be rendered in the hover tooltip.
-//         //          */
-//         //         // const createResult: (/*metadata?: Metadata*/) => sourcegraph.Badged<sourcegraph.Hover> /* = metadata*/ => {
-//         //             // const { image, title, description } = metadata ?? {}
-
-//         //             // let markdownContent = `#### [${title || maybeURL}](${maybeURL})\n\n`
-
-//         //             // if (image || description) {
-//         //             //     markdownContent += '---\n\n'
-
-//         //             //     if (image) {
-//         //             //         markdownContent += `<img height="64" src="${image}" align="left" style="padding-right: 4px;" />`
-//         //             //     }
-
-//         //             //     if (description) {
-//         //             //         markdownContent += `\n\n${description ?? ''}`
-//         //             //     }
-//         //             // }
-
-//         //             // return {
-//         //             //     range,
-//         //             //     contents: {
-//         //             //         value: maybeURL,
-//         //             //         kind: sourcegraph.MarkupKind.Markdown,
-//         //             //     },
-//         //             // }
-//         //         // }
-
-//         //         // yield link-only preview before trying to get metadata
-//         //         // createResult()
-
-//         //         // try {
-//         //         //     const settings = sourcegraph.configuration.get<Settings>().value
-
-//         //         //     const response = await fetch(
-//         //         //         `${
-//         //         //             settings['linkPreviewExpander.corsAnywhereUrl']?.replace(/\/$/, '') ??
-//         //         //             'https://cors-anywhere.herokuapp.com'
-//         //         //         }/${maybeURL}`,
-//         //         //         { cache: 'force-cache' }
-//         //         //     )
-//         //         //     const htmlString = await response.text()
-
-//         //         //     yield createResult(getMetadataFromHTMLString(htmlString))
-//         //         // } catch {
-//         //         //     // noop. already yielded link w/out metadata
-//         //         // }
-//         //     },
-//         // })
-        
-        
-        
-       
-//     )
-// }
-
-// Sourcegraph extension documentation: https://docs.sourcegraph.com/extensions/authoring
